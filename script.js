6b1cb54c526a5d9c98f64caf415d987bfd420dde
singhvidisha-alt <singhvidisha035@gmail.com>
2025-09-15 11:56:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // You can add more complex functionality later
 console.log("Telemedicine Dashboard Loaded");
+function changeLanguage(lang) {
+    alert("Language changed to: " + lang);
+    // Later you can add translations here
+}
