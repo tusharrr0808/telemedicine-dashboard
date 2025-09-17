296ad92103ca337e77af75a7bac64a4e360cfd54
singhvidisha-alt <singhvidisha035@gmail.com>
2025-09-17 18:34:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function changeLanguage(lang) {
     alert("Language changed to: " + lang);
     // Later you can add translations here
 }
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("profileForm");
+
+    if (form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const name = document.getElementById("name").value.trim();
+            const phone = document.getElementById("phone").value.trim();
+
+            if (!name || !phone) {
+                alert("Name and Phone Number are compulsory!");
+                return;
+            }
+
+            alert("Profile saved successfully!");
+            form.reset();
+        });
+    }
+});
