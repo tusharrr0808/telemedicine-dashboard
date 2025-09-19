8d9c68f428c658978c7636b4195726218fd46af9
tusharrr0808 <tmx069358@gmail.com>
2025-09-19 12:56:56 +0530

    Delete sih tele master directory

--- sih tele master/script.js
+++
@@ -1,6 +0,0 @@
-// You can add more complex functionality later
-console.log("Telemedicine Dashboard Loaded");
-function changeLanguage(lang) {
-    alert("Language changed to: " + lang);
-    // Later you can add translations here
-}
